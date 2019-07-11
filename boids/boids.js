47e83f71e0062676f7f2fc9ffe613be74ff316a1
ivar <istangeby@gmail.com>
2019-07-11 14:35:13 +0200

    Initial commit

---
+++ boids/boids.js
@@ -0,0 +1,167 @@
+const b = (boids) => {
+
+    let NUM_ENTITIES = 100;
+    let G = 20;
+    let DT = 1;
+    let boid_container = [];
+    let seed;
+    let bg;
+    let fg;
+    let c;
+    let vlim = 5;
+
+    boids.setup = () => {
+        seed = boids.random(0, 99999);
+        boids.randomSeed(seed);
+
+        // bg = random(0, 255);
+        bg = 255;
+        fg = 0;
+
+        boids.background(255);
+        c = boids.createCanvas(400, 400);
+        boids.initialize_entites();
+    };
+
+    boids.update_position = function () {
+        for (let i = 0; i < boid_container.length; i++) {
+            let center_of_mass = boids.createVector(0, 0);
+            let center_of_velo = boids.createVector(0, 0);
+            let repelling = boids.createVector(0, 0);
+
+            for (let j = 0; j < boid_container.length; j++) {
+                if (i !== j) {
+                    center_of_mass.add(boid_container[j].p);
+                    center_of_velo.add(boid_container[j].v);
+                    if (p5.Vector.sub(boid_container[j].p, boid_container[i].p).mag() < 5) {
+                        repelling.sub(p5.Vector.sub(boid_container[j].p, boid_container[i].p));
+                    }
+                }
+            }
+            center_of_mass.mult(1 / (boid_container.length - 1));
+            center_of_velo.mult(1 / (boid_container.length - 1));
+
+            let dir = p5.Vector.sub(center_of_mass, boid_container[i].p);
+            let v1 = p5.Vector.mult(dir, 1 / 1000);
+            let v2 = repelling;
+            let v3 = p5.Vector.mult(p5.Vector.sub(center_of_velo, boid_container[i].v), 1 / 16);
+
+            boid_container[i].v.add(v1);
+            boid_container[i].v.add(v2);
+            boid_container[i].v.add(v3);
+            boid_container[i].p.add(boid_container[i].v);
+            boid_container[i].soft_bound_position();
+            boid_container[i].bound_velocity();
+        }
+    };
+    boids.clamp_position = function () {
+        for (let i = 0; i < boid_container.length; i++) {
+            if (boid_container[i].p.x < 0) {
+                boid_container[i].p.x = 0;
+            }
+            if (boid_container[i].p.x > boids.width) {
+                boid_container[i].p.x = boids.width;
+            }
+            if (boid_container[i].p.y < 0) {
+                boid_container[i].p.y = 0;
+            }
+            if (boid_container[i].p.y > boids.height) {
+                boid_container[i].p.y = boids.height;
+            }
+        }
+    };
+    boids.draw = () => {
+        boids.background(255);
+        for (let i = 0; i < boid_container.length; i++) {
+            boid_container[i].draw();
+        }
+
+        boids.update_position();
+        // boids.clamp_position();
+    };
+
+    class Boid {
+        constructor(position, mass) {
+            this.p = position;
+            this.s = mass;
+            this.m = mass;
+            this.a = boids.createVector(0, 0);
+            this.v = boids.createVector(0, 0);
+        }
+
+        draw() {
+            boids.fill(0, 0, 0);
+            boids.stroke(0, 0, 0);
+            boids.ellipse(this.p.x, this.p.y, 5, 5);
+        }
+
+        set_target(target) {
+            this.t = target;
+        }
+
+        step_acceleration() {
+            var diff = p5.Vector.sub(this.t.p, this.p);
+            var mag = diff.mag();
+            var dir = diff.normalize();
+            var F = G * this.t.m / mag ** 2;
+
+            this.a = p5.Vector.mult(dir, F);
+        }
+
+        step_position() {
+            this.v.add(this.a.mult(DT));
+            this.p.add(this.v.mult(DT));
+
+            this.enforce_periodic_boundary();
+        }
+
+        enforce_periodic_boundary() {
+            if (this.p.x > boids.width || this.p.x < 0) {
+                this.p.x = this.p.x % boids.width;
+            }
+            if (this.p.y > boids.height || this.p.y < 0) {
+                this.p.y = this.p.y % boids.height;
+            }
+        }
+
+        soft_bound_position() {
+            let bounding_vector = boids.createVector(0, 0);
+            if (this.p.x < 0) {
+                bounding_vector.x = 5;
+            } else if (this.p.x > boids.width) {
+                bounding_vector.x = -5;
+            }
+            if (this.p.y < 0) {
+                bounding_vector.y = 5;
+            } else if (this.p.y > boids.height) {
+                bounding_vector.y = -5;
+            }
+            this.v.add(bounding_vector);
+        }
+
+        bound_velocity() {
+            if (this.v.mag() > vlim) {
+                this.v = this.v.normalize().mult(vlim);
+            }
+        }
+    }
+
+    boids.initialize_entites = () => {
+        for (let i = 0; i < NUM_ENTITIES; i++) {
+            let position = boids.createVector(boids.random(0, boids.height), boids.random(0, boids.width));
+            let mass = boids.random(5, 20);
+            boid_container.push(new Boid(position, mass));
+        }
+        console.log(boid_container);
+    };
+
+
+    boids.keyPressed = () => {
+
+        if (boids.keyCode == 83) {
+            boids.saveCanvas(c, seed + "_boids", "jpg");
+        }
+    }
+};
+
+let myBoids = new p5(b, "boids");
---
+++ orbits/orbits.js
@@ -0,0 +1,128 @@
+const s = (orbits) => {
+
+    let NUM_ENTITIES = 20;
+    let G = 20;
+    let DT = 1;
+    let entities = [];
+    let seed;
+    let bg;
+    let fg;
+    let c;
+
+    orbits.setup = () => {
+        seed = orbits.random(0, 99999);
+
+
+        orbits.randomSeed(seed);
+
+        // bg = random(0, 255);
+        bg = 255;
+        fg = 0;
+
+        orbits.background(0);
+        c = orbits.createCanvas(400, 400);
+        orbits.initialize_entites();
+    };
+
+    orbits.draw = () => {
+
+        for (let i = 0; i < entities.length; i++) {
+            entities[i].draw();
+        }
+        for (let i = 0; i < entities.length; i++) {
+            entities[i].step_acceleration();
+        }
+        for (let i = 0; i < entities.length; i++) {
+            entities[i].step_position();
+        }
+
+        orbits.merge_entities_too_close();
+    };
+
+    class Entity {
+        constructor(position, mass) {
+            this.p = position;
+            this.s = mass;
+            this.m = mass;
+            this.a = orbits.createVector(0, 0);
+            this.v = orbits.createVector(0, 0);
+        }
+
+        draw() {
+            // fill(255);
+            // ellipse(this.p.x, this.p.y, 2, 2);
+            orbits.stroke(10, 80, 140, 10);
+            orbits.line(this.p.x, this.p.y, this.t.p.x, this.t.p.y);
+        }
+
+        set_target(target) {
+            this.t = target;
+        }
+
+        step_acceleration() {
+            var diff = p5.Vector.sub(this.t.p, this.p);
+            var mag = diff.mag();
+            var dir = diff.normalize();
+            var F = G * this.t.m / mag ** 2;
+
+            this.a = p5.Vector.mult(dir, F);
+        }
+
+        step_position() {
+            this.v.add(this.a.mult(DT));
+            this.p.add(this.v.mult(DT));
+
+            this.enforce_periodic_boundary();
+        }
+
+        enforce_periodic_boundary() {
+            if (this.p.x > orbits.width || this.p.x < 0) {
+                this.p.x = this.p.x % orbits.width;
+            }
+            if (this.p.y > orbits.height || this.p.y < 0) {
+                this.p.y = this.p.y % orbits.height;
+            }
+        }
+    }
+
+    orbits.initialize_entites = () => {
+        for (let i = 0; i < NUM_ENTITIES; i++) {
+            let position = orbits.createVector(orbits.random(0, orbits.height), orbits.random(0, orbits.width));
+            let mass = orbits.random(5, 20);
+            entities.push(new Entity(position, mass));
+        }
+
+        for (let i = 0; i < NUM_ENTITIES; i++) {
+            entities[i].set_target(entities[(i + 1) % NUM_ENTITIES]);
+        }
+    };
+
+    orbits.merge_entities_too_close = () => {
+        entities_to_remove = []
+        for (let i = 0; i < entities.length; i++) {
+            let r = orbits.abs(p5.Vector.sub(entities[i].p, entities[i].t.p).mag());
+            if (r < (entities[i].s + entities[i].t.s) / 2) {
+                entities[i].s += entities[i].t.s;
+                entities[i].m += entities[i].t.m;
+                entities[i].t = entities[i].t.t;
+                entities[i].v.add(entities[i].t.v);
+                entities[i].a.add(entities[i].t.a);
+                entities_to_remove.push(i + 1);
+                i++;
+            }
+        }
+        for (let i = 0; i < entities_to_remove.length; i++) {
+            entities.splice(entities_to_remove[i], 1);
+        }
+    };
+
+
+    orbits.keyPressed = () => {
+
+        if (orbits.keyCode == 83) {
+            saveCanvas(c, seed + "_orbits", "jpg");
+        }
+    }
+};
+
+let myOrbits = new p5(s, "orbits");
